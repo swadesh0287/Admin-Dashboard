414457cac981f3f4778572fcb931f293c8bec341
swadesh0287 <76160679+swadesh0287@users.noreply.github.com>
2024-01-04 13:28:43 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2024 Made by Swadesh Kumar
+    </p>
+  </div>
+);
+
+export default Footer;
