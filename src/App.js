6e5fec1e180ce68b9cb05e09c544f961253ab492
Swadesh Kumar <swadesh0287@gmail.com>
2024-06-20 00:57:57 +0530

    removed cycle dependency

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Sidebar, ThemeSettings } from './components';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,11 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import avatar from '../data/avatar.jpg';
-import { Cart, Chat, Notification, UserProfile } from '.';
+
+import Cart from './Cart';
+import Chat from './Chat';
+import UserProfile from './UserProfile';
+import Notification from './Notification';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
